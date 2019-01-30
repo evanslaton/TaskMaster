72b63aba54445b17f3e1f3875e6f565becca0437
Evan Slaton <evanslaton@gmail.com>
2019-01-30 13:28:28 -0800

    Added firebase login

--- app/src/main/java/com/evanslaton/taskmaster/MainActivity.java
+++ app/src/main/java/com/evanslaton/taskmaster/MainActivity.java
@@ -1,90 +1,64 @@
 package com.evanslaton.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.room.Room;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.TextView;
 
-import com.evanslaton.taskmaster.project.Project;
-import com.evanslaton.taskmaster.project.ProjectAdapter;
-import com.evanslaton.taskmaster.project.ProjectDatabase;
+import com.evanslaton.taskmaster.project.ProjectList;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 // From Google's Android docs and http://www.vogella.com/tutorials/AndroidRecyclerView/article.html
 public class MainActivity extends AppCompatActivity {
-    // Database variables
-    protected ProjectDatabase projectDatabase;
-    protected List<Project> projects = new ArrayList<>();
-
-    // Recycler View variables
-    private RecyclerView recyclerView;
-    private ProjectAdapter adapter;
-    private RecyclerView.LayoutManager layoutManager;
+    protected static final int RC_SIGN_IN = 9371;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Saves the recycler view
-        recyclerView = (RecyclerView) findViewById(R.id.projects);
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build());
 
-        // 'Creates' the database
-        projectDatabase = Room.databaseBuilder(getApplicationContext(),
-                ProjectDatabase.class, "projectDatabase")
-                .allowMainThreadQueries()
-                .fallbackToDestructiveMigration()
-                .build();
-
-        // Gets all projects from the database
-        projects = projectDatabase.projectDao().getAll();
-
-
-        // FOR TESTING ONLY
-        if (projects.size() == 0) {
-            projects.add(new Project("DoTheNeedful"));
-            projectDatabase.projectDao().insertProject(projects.get(0));
-            projects.add(new Project("DoTheNeedful2"));
-            projectDatabase.projectDao().insertProject(projects.get(1));
-            projects.add(new Project("DoTheNeedful3"));
-            projectDatabase.projectDao().insertProject(projects.get(2));
-            projects = projectDatabase.projectDao().getAll();
-        }
-
-        // Creates a layout manager and assigns it to the recycler view
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-
-        // Specifies which adapter the recycler view should use
-        adapter = new ProjectAdapter(projects);
-        recyclerView.setAdapter(adapter);
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
     }
 
-    // Adds a new project to the Project Database
-    public void createProject(View v) {
-        TextView projectTextView = findViewById(R.id.createProjectTitle);
-        String projectTitle = projectTextView.getText().toString();
-        projectDatabase.projectDao().insertProject(new Project(projectTitle));
-        projectTextView.setText(""); // Empties the input field
+    // Invoked after Firebase sign in
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        // Updates the recycler view
-        projects = projectDatabase.projectDao().getAll();
-        adapter. setProjects(projects);
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
 
-        // https://stackoverflow.com/questions/13593069/androidhide-keyboard-after-button-click/13593232 (second answer)
-        // Hides the keyboard
-        try {
-            InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
-            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
-        } catch (Exception exception) {
-            // Do nothing
+            // Checks to see if the user successfully signed in
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                // Takes the user to the project list activity
+                Intent goToProjectListIntent = new Intent(this, ProjectList.class);
+                this.startActivity(goToProjectListIntent);
+
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
         }
     }
 }
